f4a43c74399ccfe619402669f73f90595c38b2e2
Ioan Dragos Crismaru <dragoscrismaru@dedeman.ro>
2025-04-15 12:33:11 +0300

    made mock data api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 "use client";
+// import axios from "axios";
+// import { useEffect } from "react";
 
-export default function Home() {
-  return <div>Salut</div>;
+// export default function Home() {
+//   useEffect(() => {
+//     const loadData = async () => {
+//       const res = await axios.get(
+//         `${process.env.NEXT_PUBLIC_BASE_URL}/api/mock`
+//       );
+//       // console.log(awa)
+//       // const data = await res.json();
+//       console.log(res);
+//     };
+
+//     loadData();
+//   }, []);
+
+//   return <div>Salut</div>;
+// }
+
+
+export default function Home(){
+  return <>Hello world!</>
 }
