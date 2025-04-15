722bfe2f6fd822a518e2d5c6e77206ad6662a4ed
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-04-15 14:47:47 +0300

    fetching users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,48 @@
 "use client";
-// import axios from "axios";
-// import { useEffect } from "react";
 
-// export default function Home() {
-//   useEffect(() => {
-//     const loadData = async () => {
-//       const res = await axios.get(
-//         `${process.env.NEXT_PUBLIC_BASE_URL}/api/mock`
-//       );
-//       // console.log(awa)
-//       // const data = await res.json();
-//       console.log(res);
-//     };
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-//     loadData();
-//   }, []);
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-//   return <div>Salut</div>;
-// }
+export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const res = await axios.get<{ users: User[] }>(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/mock`
+        );
+        console.log("result", res.data.users);
+        setUsers(res.data.users);
+      } catch (err: any) {
+        setError(err.message || "Eroare necunoscută");
+        console.error("Eroare axios:", err);
+      }
+    };
 
-export default function Home(){
-  return <>Hello world!</>
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      {error && <div>{`Eroare: ${error}`}</div>}
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>
+            <strong>Nume:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+        </div>
+      ))}{" "}
+    </div>
+  );
 }
